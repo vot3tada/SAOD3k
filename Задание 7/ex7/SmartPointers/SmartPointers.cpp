c28c629b8cbda8840845bd2299e7b06e46879eb7
vot3tada <zindromed@bk.ru>
2022-10-27 14:01:50 +0300

    Исправил баг в 7

--- Задание 7/ex7/SmartPointers/SmartPointers.cpp
+++ Задание 7/ex7/SmartPointers/SmartPointers.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	//shared_ptr<Test[]> sp(new Test[3]);
 	/*shared_ptr<Test> sp(new Test[3], default_delete<Test[]>());
-	for (int i = 1; i <= 3; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		(sp.get() + i)->Val = i;
 		cout << (sp.get() + i)->Val << endl;
